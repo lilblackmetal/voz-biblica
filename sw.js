02125f4111e1eae720a7382f24415f83cdc71016
lilblackmetal <f34014369@gmail.com>
2026-09-12 11:26:53 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // la copia guardada como respaldo si no hay internet: así una versión nueva se
 // ve en la primera recarga. Sólo las imágenes van desde la copia guardada, que
 // es lo que hace que la app abra rápido.
-const CACHE = 'vozbiblica-v15';
+const CACHE = 'vozbiblica-v16';
 
 self.addEventListener('install', () => self.skipWaiting());
 
